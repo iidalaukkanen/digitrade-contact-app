6efcf22def49f47cfba4276f96a4a9b07d788d10
iidalaukkanen <iida.laukkanen@hotmail.com>
2019-05-22 09:28:42 +0300

    ContactsWebApp
    contact-http.service updated

--- src/app/contact/services/contact-http.service.ts
+++ src/app/contact/services/contact-http.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactHttpService {
+  private url: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.url = 'http://localhost:3000/contacts';
+  }
+
+  get(){
+    return this.http
+      .get(this.url)
+      .subscribe((data) =>{
+        console.log(data);
+      });
+  }
 }
