6a788e52f55f6f4c3b6a2d754d0f99e32ad6708b
iidalaukkanen <iida.laukkanen@hotmail.com>
2019-06-07 11:41:10 +0300

    ContactsWebApp
    enpointUrl updated

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -2,8 +2,8 @@ export class Contact {
   id: any;
   firstName: string;
   lastName: string;
-  phone: string;
-  email: string;
+  phoneNumber: string;
+  emailAddress: string;
   streetAddress: string;
   postalCode: string;
   city: string;
@@ -12,8 +12,8 @@ constructor(id?: any, firstName?: string, lastName?: string, phone?: string, ema
   this.id = id;
   this.firstName = firstName;
   this.lastName = lastName;
-  this.phone = phone;
-  this.email = email;
+  this.phoneNumber = phone;
+  this.emailAddress = email;
   this.streetAddress = streetAddress;
   this.postalCode = postalCode;
   this.city = city;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  endpointUrl: 'http://localhost:3000/'
+  endpointUrl: 'https://dtcontactapi.azurewebsites.net/api/'
 };
 
 /*
